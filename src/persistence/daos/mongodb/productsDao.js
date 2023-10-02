3b54f4a5650c26235114c9ecf8df17a980e382df
GenaroActis <genaroactis3@gmail.com>
2023-10-02 12:52:00 -0300

    upd

--- src/persistence/daos/mongodb/productsDao.js
+++ src/persistence/daos/mongodb/productsDao.js
@@ -1,6 +1,5 @@
 import { ProductsModel } from './models/productsModels.js';
 import { MockingProdsModel } from './models/mockingProdsModel.js'
-import { generateProducts } from '../../../utils/utils.js'
 import logger from '../../../utils/logger.js'
 export default class ProductsDaoMongoDB {
     async getAllProducts(page = 1, limit = 5, key, value, sortField = 'price', sortOrder = 'desc') {
@@ -74,37 +73,6 @@ export default class ProductsDaoMongoDB {
         } catch (error) {
             logger.error(error)
             throw new Error(error);
-        };
-    };
-    async createMockingProduct(quantity = 100) {
-        try {
-        const productsArray = []
-        for (let i = 0; i < quantity; i++) {
-            const prod = generateProducts();
-            productsArray.push(prod);
-        }
-        const response = await MockingProdsModel.create(productsArray);
-        return response;
-        } catch (error) {
-        logger.error(error)
-        throw new Error(error);
-        };
-    };
-    async getAllMockingProduct(page = 1, limit = 5, key, value, sortField = 'price', sortOrder = 'desc') {
-        try {
-            const query = {};
-            if (key && value ) {
-                query[key] = value;
-            };
-            const options = {page, limit, sort: {}}
-            if (sortField && sortOrder) {
-                options.sort[sortField] = sortOrder;
-            };
-            const response = await MockingProdsModel.paginate(query, options);
-            return response;
-        } catch (error) {
-        logger.error(error)
-        throw new Error(error);
         };
     };
 };
--- src/routes/apiRoutes/productsRouter.js
+++ src/routes/apiRoutes/productsRouter.js
@@ -6,8 +6,6 @@ import {
     deleteProductController,
     updateProductController,
     getProductBySomethingController,
-    getMockingProdsController,
-    createMockingProdsController
 } from '../../controllers/productController.js';
 import { checkAuth } from '../../jwt/auth.js';
 const router = Router();
@@ -15,8 +13,6 @@ import { ensureIsAdmOrPrem } from '../../middlewares/ensureIsAdmOrPrem.js'
 import { validateCreateProduct } from '../../middlewares/validators/productValidator.js'
 import { uploadProd } from '../../middlewares/multer.js'
 
-router.get('/mocking', getMockingProdsController);
-router.post('/mocking', createMockingProdsController);
 router.get('/', checkAuth,  getAllProductsController)
 router.get('/:id', checkAuth, getProductByIdController);
 router.post('/', checkAuth, ensureIsAdmOrPrem, uploadProd.single('img'), createProductController);
