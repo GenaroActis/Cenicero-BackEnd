4ddc9f80bc9e65e0c274f85097780a88a55fdc94
GenaroActis <genaroactis3@gmail.com>
2023-10-02 12:11:39 -0300

    2

--- src/utils/deleteInactiveUsers.js
+++ src/utils/deleteInactiveUsers.js
@@ -9,12 +9,12 @@ const deleteInactiveUsers = async () => {
         const millisecondsInADay = 24 * 60 * 60 * 1000;
         const period = millisecondsInADay * 7
         const limit = new Date(Date.now() - period)
-        const deletedUsers = await UserModel.find({ lastActivity: { $lt: limit } })
-        console.log('delet', deletedUsers)
+        // const deletedUsers = await UserModel.find({ lastActivity: { $lt: limit } })
+        // console.log('delet', deletedUsers)
         const res = await UserModel.deleteMany({lastActivity: {$lt: limit}})
-        deletedUsers.map((user) =>{
-            sendEmailDeletedAccountController(user.email)
-        })
+        // deletedUsers.map((user) =>{
+        //     sendEmailDeletedAccountController(user.email)
+        // })
         return res
     } catch (error) {
         console.log(error)
